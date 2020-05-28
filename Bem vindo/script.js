470fca3ac2d535d642caf84ea56dfe7ed243a784
Gabriel Segatto <gabriel.segatto58@gmail.com>
2020-05-28 16:40:53 -0300

    add bem vindo, mov requi. e conf.
    arquivos das paginas requisitos e confrimação movidos da pasta root para a pasta requisitor e confirmação

---
+++ Bem vindo/script.js
@@ -0,0 +1,15 @@
+function login(){
+    const user = document.getElementById('user');
+    const password = document.getElementById('password');
+    var userText = user.value.toLowerCase();
+
+    if(userText !== ''){ 
+        localStorage.setItem('user', userText);
+    } if (password !== ''){     
+        localStorage.setItem('password', password.value);
+    }
+
+    console.log(localStorage.getItem('user'));
+    console.log(localStorage.getItem('password'));
+
+}
